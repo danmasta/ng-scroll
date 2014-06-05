e3be720a4e984e98c22beb7bf0d6b3bf8ecd6dc3
Daniel Smith <daniel.smith@bedrosians.com>
2014-06-05 13:34:40 -0700

    add demo

---
+++ demo/app.js
@@ -0,0 +1,20 @@
+var app = angular.module('app', ['ngScroll']);
+
+app.directive('scrollTest', ['scroll', function(scroll){
+  return{
+    restrict:'A',
+    controller: function($scope){
+      scroll.set();
+      $scope.$on('scroll', function(data, $event){
+        $scope.scroll = $event;
+      });
+    },
+    template:'<li data-test-item ng-repeat="item in testitems">{{item.title}}</li>',
+    link:function($scope, $element, $attribute){
+      $scope.testitems = [];
+      for( var i = 0; i < 500; i++){
+        $scope.testitems.push({title:'test item ' + i, value:i});
+      }
+    }
+  };
+}]);
